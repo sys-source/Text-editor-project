725d1d74f9b63c0bb6344de7d737feb642a10fb8
sys-source <anmaryashin@gmail.com>
2024-11-07 16:48:46 +0100

    Add files via upload

---
+++ main txt edit.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+class Settings:
+    ... # controls all the settings of the programm
+
+class Window_Widgets:
+    window = Tk()                                   # creating window
+    window.geometry("900x600")                      # changing window size
+    window.mainloop()                               # showing window
+
+class Input:
+    ... # While looooop waits for input
+
+class Output:
+    ... # Takes Input or File I/O  and prints it out 
+
+class Saver:
+    ... # when programm gets closed reads all the stuff on the screen and takes all the settings and saves them File I/O

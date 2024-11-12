2cd8fa207df7a280871c3599ae764a16e2ce8df1
Nino <158091370+nino1936nino@users.noreply.github.com>
2024-11-12 14:31:08 +0100

    basic saving

--- raw.py
+++ raw.py
@@ -1,21 +1,31 @@
 from tkinter import *
-
+from pynput import keyboard
+import os
 # Main variebles
 dark_theme = ["#21201e", "#e3e0da", "#131414"]
 light_theme = ["#e3e0da", "#000000", "#babfbf"]
 Output = ""
 size = 25
-LIGHT_THEME_SETTINGS_BUTTON_IMAGE = PhotoImage(file=...)    # Settings button (light theme)        
-DARK_THEME_SETTINGS_BUTTON_IMAGE = PhotoImage(file=...)     # Settings button (dark theme)  
+
+window = Tk()
+window.title("Goofy ahh text editor")
+
+
+LIGHT_THEME_SETTINGS_BUTTON_IMAGE = PhotoImage(file="Screenshot 2024-11-01 191922.png")    # Settings button (light theme)        
+DARK_THEME_SETTINGS_BUTTON_IMAGE = PhotoImage(file="Screenshot 2024-11-01 185621.png")     # Settings button (dark theme)  
 
 # Defining commands for buttons
 def open_settings():
     settings_label = Label(window, bg=dark_theme[2], padx=300, pady=400)
     settings_label.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-# Creating window
-window = Tk()
-window.title("Goofy ahh text editor")
+
+
+
+        
+        
+        
+# Start the keyboard listener
 
 # Creating text area and showing it
 text = Text(window, fg=dark_theme[1], bg=dark_theme[0],font=("Calibri", 25), padx=200, pady=100)
@@ -31,8 +41,22 @@ settings_button = Button(window, command=open_settings, image=DARK_THEME_SETTING
 # Getting input (text on the screen)
 input = text.get("1.0", END)
 
+
+def on_press(key):
+    global Output
+    try:
+        # If a printable character is pressed
+        if hasattr(key, 'char') and key.char is not None:
+            Output += key.char
+        elif key == keyboard.Key.f5:
+            with open(os.getcwd() + '\ luanonna.txt' ,'a') as f:
+                f.write(input)
+                print(os.getcwd())
+                print(input)
+
+    except Exception as e:
+        print(f"Error: {e}")
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
 # Looooooop
 window.mainloop()
-
-
-# Note the code is not usable in this form in order to run the code you need to make setting button and it´s veriebles a comment !!!

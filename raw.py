6b99d23573fc1e171bf0cf5418299026b2ef1bbe
sys-source <anmaryashin@gmail.com>
2024-11-16 15:19:38 +0100

    Update raw.py

--- raw.py
+++ raw.py
@@ -12,11 +12,28 @@ window = Tk()
 window.title("Goofy ahh text editor")
 
 
-LIGHT_THEME_SETTINGS_BUTTON_IMAGE = PhotoImage(file=r"Screenshot 2024-11-01 185621.png")    # Settings button (light theme)        
-DARK_THEME_SETTINGS_BUTTON_IMAGE = PhotoImage(file=r"Screenshot 2024-11-01 191922.png")     # Settings button (dark theme)  
+LIGHT_THEME_SETTINGS_BUTTON_IMAGE = PhotoImage(file=r"C:\Users\anmar\Downloads\Screenshot 2024-11-01 185621.png")    # Settings button (light theme)              
+DARK_THEME_SETTINGS_BUTTON_IMAGE = PhotoImage(file=r"C:\Users\anmar\Downloads\Screenshot 2024-11-01 191922.png")     # Settings button (dark theme) 
 
 # Defining commands for buttons
+
+def close_settings():
+    # Destroy each widget created in the open_settings function
+    settings_label.destroy()
+    settings_label_for_settings.destroy()
+    settings_label_for_font_size.destroy()
+    settings_label_for_font.destroy()
+    settings_label_for_theme.destroy()
+    settings_button_for_closing.destroy()
+    settings_button_for_font.destroy()
+
+
 def open_settings():
+    global settings_label, settings_label_for_settings
+    global settings_label_for_font_size, settings_label_for_font
+    global settings_label_for_theme, settings_button_for_closing
+    global settings_button_for_font
+
     # Creating the settings window 
     settings_label = Label(window, bg=dark_theme[2], width=90, height=55)
     settings_label.place(relx=0.5, rely=0.5, anchor=CENTER)
@@ -26,18 +43,23 @@ def open_settings():
     settings_label_for_settings.place(relx=0.5, rely=0.15, anchor=CENTER)
 
     # creating the texts next to the buttons
-    settings_label_for_font = Label(window, bg=dark_theme[2], fg=dark_theme[1], text="Font:", font=("Ink free", 23), width=10, height=1)
-    settings_label_for_font.place(relx=0.38, rely=0.22)
-
     settings_label_for_font_size = Label(window, bg=dark_theme[2], fg=dark_theme[1], text="Font size:", font=("Ink free", 23), width=10, height=1)
-    settings_label_for_font_size.place(relx=0.38, rely=0.45)
+    settings_label_for_font_size.place(relx=0.38, rely=0.22)
+
+    settings_label_for_font = Label(window, bg=dark_theme[2], fg=dark_theme[1], text="Font:", font=("Ink free", 23), width=10, height=1)
+    settings_label_for_font.place(relx=0.38, rely=0.45)
 
     settings_label_for_theme = Label(window, bg=dark_theme[2], fg=dark_theme[1], text="Theme:", font=("Ink free", 23), width=10, height=1)
     settings_label_for_theme.place(relx=0.38, rely=0.68)
 
     #Creating the buttons
-    settings_button_for_font = Button(window, bg=dark_theme[0], text=size, fg=dark_theme[1], font=(30), height=2, width=20)
+    settings_button_for_closing = Button(window, bg=dark_theme[2], text="×", font=("Ink free", 20), fg=dark_theme[1], width=5, height=2, borderwidth=0, activebackground=dark_theme[2], activeforeground=dark_theme[1], command=close_settings)
+    settings_button_for_closing.place(relx=0.624, rely=0.09)
+
+    settings_button_for_font = Button(window, bg=dark_theme[0], text=size, fg=dark_theme[1], font=(30), height=2, width=20, activebackground=dark_theme[0], activeforeground=dark_theme[1])
     settings_button_for_font.place(relx=0.56, rely=0.24, anchor=CENTER)
+
+
 
 # Getting input (text on the screen)
 def save_file():
@@ -91,3 +113,4 @@ listener = keyboard.Listener(on_press=on_press)
 listener.start()
 # Looooooop
 window.mainloop()
+
